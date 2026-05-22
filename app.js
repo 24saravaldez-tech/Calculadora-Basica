9647ed470773aaf9451e488d47e2d21114e80247
Sara Valdéz <24sara.valdez@gmail.com>
2026-05-22 17:47:35 -0600

    cambios a realizar

--- app.js
+++ app.js
@@ -1,46 +1,17 @@
 
 let botonNumero = document.querySelectorAll('.boton-numero')
 let input = document.querySelector('.salida')
-let operaciones = document.querySelectorAll('#operadores')
+let operaciones = document.querySelectorAll('.operador')
 
 botonNumero.forEach(btn => {
-    btn.addEventListener('click', (event) =>{
+    btn.addEventListener('click', (event) => {
         input.value += event.target.textContent
     })
 })
 
-operaciones.forEach
+operaciones.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        event.target.textContent.includes()
 
-
-
-
-
-
-
-
-
-
-
-// let uno = document.querySelector('#uno')
-// uno.setAttribute('valor', '1')
-
-// let botonNumeros = [uno]
-
-
-// // const imprimirNumeros = () => {
-// //     botonNumeros.addEventListener('click', (event) => {
-//         let resultado = botonNumeros[]
-// //         return resultado
-// //     })
-// // }
-
-// console.log(resultado)
-
-// let resultado = ''
-
-// 
-
-
-// console.log(botonNumero)
-
-
+    })
+})
